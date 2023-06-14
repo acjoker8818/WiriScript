041ed4c0a78f67c0d92797e5660f1ed4ab76cebb
acjoker8818 <112934795+acjoker8818@users.noreply.github.com>
2023-06-14 06:59:23 -0500

    Add files via upload

--- WiriScript.lua
+++ WiriScript.lua
@@ -7,7 +7,7 @@ THIS FILE IS PART OF WIRISCRIPT
 
 ---@diagnostic disable: local-limit
 local scriptStartTime = util.current_time_millis()
-gVersion = 29.5
+gVersion = 29.51
 util.require_natives(1663599433)
 
 local required <const> = {
